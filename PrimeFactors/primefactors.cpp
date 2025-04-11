bd02e5e1b51ad582b5cad6ff3740a1ed502ddf52
heejeong <irene725k@gmail.com>
2025-04-11 14:09:20 +0900

    [feature] 48p

--- PrimeFactors/primefactors.cpp
+++ PrimeFactors/primefactors.cpp
@@ -11,6 +11,10 @@ class PrimeFactor {
           result.push_back(2);
           number /= 2;
         }
+
+      } else if (number == 6) {
+        result.push_back(2);
+        result.push_back(3);
       } else
         result.push_back(number);
     }
